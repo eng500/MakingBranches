55ad7fea0a26cbfa5551b9ba337062ffeb53d9f3
Andrea <awang51@stuy.edu>
2024-05-17 09:10:46 -0400

    first commit

--- Test.java
+++ Test.java
@@ -1,4 +1,10 @@
 public class Test{
   //insert code here
-  
+  public static void main(String[] args){
+    String name = "Andrea";
+
+    String course = "APCS";
+
+    System.out.println(name + " is in " + course);
+  }
 }
