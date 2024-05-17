57bc2e68320c26b9d7e436f1d8228a1cd730e411
Emily Ng <eng50@stuy.edu>
2024-05-17 09:16:16 -0400

    emily's branch

--- Test.java
+++ Test.java
@@ -1,4 +1,10 @@
 public class Test{
   //insert code here
-  
+  public static void main (String[] args){
+    String name = "emily";
+
+    String school = "APCS";
+
+    System.out.println(name + " is in " + school);
+  }
 }
